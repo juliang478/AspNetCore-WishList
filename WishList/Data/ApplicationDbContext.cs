5cd555bf54fbe0742e162a92c7989187fde11986
Julian Galvan <juliang478@email.phoenix.edu>
2019-08-22 09:36:59 -0700

    Completed Module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WishList.Models;
 
 namespace WishList.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<Item> Items { get; set; }
     }
 }
+
