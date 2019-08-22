cd2537f0b6343bf2c98a2c399dc4b602871a1bfa
Julian Galvan <juliang478@email.phoenix.edu>
2019-08-22 09:40:33 -0700

    Completed Module

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -9,6 +9,13 @@ namespace WishList.Data
 {
     public class ApplicationDbContext : DbContext
     {
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
         public DbSet<Item> Items { get; set; }
     }
 }
